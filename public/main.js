6b2aa1250d5cf1921dcc607a39a7e33c9ff2cd56
valerio-pescatori <pescatorim14@gmail.com>
2021-09-22 16:56:10 +0200

    installed Clamp-js module

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+import $clamp from "../node_modules/clamp-js/clamp.js";
+// come cazzo se fa sto import
 var btn = document.getElementById("red-button");
 var circle = document.getElementById("circle");
 
@@ -13,4 +15,15 @@ btn.onclick = (e) => {
   setTimeout(() => {
     btn.classList.toggle("animate-btn");
   }, 500);
+};
+
+document.onload = (e) => {
+  var toBeClamped = document.getElementById("clamp-me");
+  var clampThose = document.getElementsByClassName("card-title text-brea");
+
+  $clamp(toBeClamped, { clamp: 2 });
+  console.log("test");
+  // clampThose.forEach((element) => {
+  //   $clamp(element, { clamp: 2 });
+  // });
 };
