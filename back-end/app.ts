223a415094e52f4109e379d3f381afc851f7b018
valerio-pescatori <pescatorim14@gmail.com>
2022-04-25 20:29:30 +0200

    playing with components fixed port for produciton mode

--- back-end/app.ts
+++ back-end/app.ts
@@ -6,7 +6,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 //rendo statica public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(app.get('port'), () =>
   console.log(`Server started on port ${app.get('port')}`)
