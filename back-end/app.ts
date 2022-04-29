b3a99143090a2c1029671208174679c83f7e3c1b
valerio-pescatori <pescatorim14@gmail.com>
2022-04-29 17:08:09 +0200

    fa finally works

--- back-end/app.ts
+++ back-end/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 //rendo statica public
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../front-end/assets/')));
 
 app.listen(app.get('port'), () =>
   console.log(`Server started on port ${app.get('port')}`)
